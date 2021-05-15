07e54460f1cfaec85ccbea85143ec3252973668b
Trivikram-Sharma <vickygosukonda@gmail.com>
2021-05-15 16:01:27 +0000

    Changes made from third party lab

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   //Alert if principal amount is not positive
+   if(principal<=0){
+       alert("Enter a positive number");
+       document.getElementById("principal").focus();
+   }
+
+   var interest = (principal * years * rate) /100;
+   var year = new Date().getFullYear() + parseInt(years);
+   var amount = parseInt(principal)+parseFloat(interest);
+   var result = document.getElementById("result");
+   result.innerHTML = "If you deposit "+ principal+",\<br\> at an interest rate of "+rate+".\<br\> You will receive an amount of "+ interest +",\<br\> in the year "+year +"\<br\>";
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
+
 }
         
