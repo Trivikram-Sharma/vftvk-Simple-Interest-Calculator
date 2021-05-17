62ea6795069c9c6d0973687e692444163de036e3
Trivikram Sharma <vickygosukonda@gmail.com>
2021-05-17 09:07:17 +0530

    Made changes to javascript input validation

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ function compute()
        alert("Enter a positive number");
        document.getElementById("principal").focus();
    }
+   else{
 
    var interest = (principal * years * rate) /100;
    var year = new Date().getFullYear() + parseInt(years);
    var amount = parseInt(principal)+parseFloat(interest);
    var result = document.getElementById("result");
    result.innerHTML = "If you deposit "+ principal+",\<br\> at an interest rate of "+rate+".\<br\> You will receive an amount of "+ interest +",\<br\> in the year "+year +"\<br\>";
+   }
 }
 
 function updateRate(){
